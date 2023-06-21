9f25d208b362c0b2e7b01a5631987f5936c0d215
AliZia3 <alizia0217@gmail.com>
2023-06-21 15:25:26 -0400

    Hero section complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,23 +11,26 @@ const Navbar = () => {
 	const [ toggle, setToggle ] = useState(false);
 
 	return (
+		// Full Nav Bar
 		<nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`} >
-			
+			{/* Navbar Content */}
 			<div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
 				
-				<Link to="/" className="flex items-center gap-2" onClick={() => { setActive(""); window.scroll(0, 0); }} >
-					<img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-					<p className="text-white text-[18px] font-bold cursor-pointer flex">Ali Zia</p>
+				{/* Logo */}
+				<Link to="/" className="flex items-center" onClick={() => { setActive(""); window.scroll(0, 0); }} >
+					<img src={logo} alt="logo" className="w-12 h-12 object-contain" />
 				</Link>
 
-				<ul className="list-none hidden sm:flex flew-row gap-10">
+				{/* Navbar Links */}
+				<ul className="list-none hidden sm:flex flew-row gap-24">
 					{navLinks.map((link) => (
 						<li key={link.id} className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} onClick={() => setActive(link.title)}>
 							<a href={`#${link.id}`}>{link.title}</a>
 						</li>
 					))}
 				</ul>
-
+				
+				{/* Burger Menu */}
 				<div className="sm:hidden flex flex-1 justify-end items-center">
 					<img src={toggle ? close : menu} alt='menu' className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={() => setToggle(!toggle)} />
 					<div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
