f5e0d5c42b7eae382e2887eddc9204593e65fda4
AliZia3 <alizia0217@gmail.com>
2023-06-25 23:16:00 -0400

    Updated navbar (burger menu)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
 				</Link>
 
 				{/* Navbar Links */}
-				<ul className="list-none hidden sm:flex flew-row gap-24">
+				<ul className="hidden lg:flex gap-28">
 					{navLinks.map((link) => (
 						<li key={link.id} className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} onClick={() => setActive(link.title)}>
 							<a href={`#${link.id}`}>{link.title}</a>
@@ -31,20 +31,22 @@ const Navbar = () => {
 				</ul>
 
 				{/* Burger Menu */}
-				<div className="sm:hidden flex flex-1 justify-end items-center">
-					<img src={toggle ? close : menu} alt='menu' className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={() => setToggle(!toggle)} />
-					<div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-						<ul className="list-none flex justify-end items-start flex-col gap-4">
-							{navLinks.map((link) => (
-								<li key={link.id} className={`${active === link.title ? "text-white" : "text-secondary"} font-poppins font-medium cursor-pointer text-[16px]`} onClick={() => {
-									setActive(link.title)
-									setToggle(!toggle)
-								}}>
-									<a href={`#${link.id}`}>{link.title}</a>
-								</li>
-							))}
-						</ul>
-					</div>
+				<div className="lg:hidden flex justify-end items-center">
+					
+					<img src={toggle ? close : menu} alt='menu' className="w-[45px] h-[28px] object-contain cursor-pointer z-10 absolute" onClick={() => setToggle(!toggle)} />
+					
+					{toggle ? (
+						<ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800">
+						{navLinks.map((link) => (
+							<li key={link.id} className={`${active === link.title ? "text-white text-5xl" : "text-secondary"} font-poppins font-medium cursor-pointer text-4xl px-4 capitalize py-6`} onClick={() => {
+								setActive(link.title)
+								setToggle(!toggle)
+							}}>
+								<a href={`#${link.id}`}>{link.title}</a>
+							</li>
+						))}
+					</ul>
+					) : ""}
 				</div>
 
 			</div>
