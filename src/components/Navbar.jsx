5a920a63b312109606d062f0cf8237c9151f8163
AliZia3 <alizia0217@gmail.com>
2023-07-04 17:58:05 -0400

    Updated navbar (Removed duration hovers)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -65,14 +65,14 @@ const Navbar = () => {
 			<div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
 
 				{/* Logo */}
-				<Link to="/" className="flex items-center z-20" onClick={() => { setActive(""); window.scroll(0, 0); setToggle(!toggle) }} >
+				<Link to="/" className="flex items-center z-20" onClick={() => { setActive(""); window.scroll(0, 0); {toggle ? setToggle(!toggle) : ''} }} >
 					<img src={logo} alt="logo" className="w-12 h-12 object-contain" />
 				</Link>
 
 				{/* Navbar Links */}
 				<ul className="hidden lg:flex gap-x-24">
 					{navLinks.map((link) => (
-						<li key={link.id} className={`${active === link.title ? "text-white border-b-2 duration-0" : "text-secondary border-b-0 duration-0"} hover:text-blue-500 hover:scale-95 hover:opacity-80 duration-300 ease-in-out text-[18px] font-medium cursor-pointer`} onClick={() => setActive(link.title)}>
+						<li key={link.id} className={`${active === link.title ? "text-white border-b-2" : "text-secondary border-b-0"}  border-blue-500 hover:text-blue-500 hover:scale-95 hover:opacity-80 ease-in-out text-[18px] font-medium cursor-pointer`} onClick={() => setActive(link.title)}>
 							<a href={`#${link.id}`}>{link.title}</a>
 						</li>
 					))}
@@ -84,9 +84,9 @@ const Navbar = () => {
 					<img src={toggle ? close : menu} alt='menu' className="w-[45px] h-[28px] object-contain cursor-pointer z-10 absolute" onClick={() => setToggle(!toggle)} />
 
 					{toggle ? (
-						<ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b" style={{ background: 'linear-gradient(to bottom, rgb(3, 0, 10), rgb(8, 20, 77))' }}>
+						<ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b" style={{ background: 'linear-gradient(to bottom, rgb(5, 13, 48), rgb(3, 10, 38))' }}>
 							{navLinks.map((link) => (
-								<li key={link.id} className={`${active === link.title ? "text-white" : "text-secondary"} font-poppins font-medium cursor-pointer text-3xl px-4 capitalize py-6`} onClick={() => {
+								<li key={link.id} className={`${active === link.title ? "text-white text-4xl" : "text-secondary"} font-poppins font-medium cursor-pointer text-3xl px-4 capitalize py-6`} onClick={() => {
 									setActive(link.title)
 									setToggle(!toggle)
 								}}>
